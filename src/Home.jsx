4741a1bd92f8185a7e67e229bf5ca44c748d6850
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-02-21 15:50:19 -0500

    commiting changes to the home page search

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,86 @@
+/* eslint-disable array-callback-return */
 import React, { Component } from 'react';
-import { Form, Col } from "react-bootstrap";
-import Select from "react-select";
 
 
 class Home extends Component {
-    
-    state = {
-        options: [],
-        selectedOptionTicker: null
-    };
+  state = {
+    options: [],
+    selectedOptionTicker: null,
+    filteredItems: [],
+    search: ""
+  };
 
   showTickerOptions = () => {
-    // console.log(this.props.tickers);
+    console.log(this.props.tickers);
     let tickerOptions = { ...this.props.tickers };
     let tickerList = [];
     for (let i in tickerOptions) {
-      tickerList.push({value: tickerOptions[i].symbol, label: tickerOptions[i].name});
+      tickerList.push({
+        value: tickerOptions[i].symbol,
+        label: tickerOptions[i].name
+      });
     }
     console.log(tickerList);
-    return tickerList.slice(0,1000);
+    return tickerList.slice(0, 1000);
   };
 
-  handleChangeTicker = selectedOptionTicker => {
-    console.log(selectedOptionTicker.target);
+  // handleChangeTicker = selectedOptionTicker => {
+  //   console.log(selectedOptionTicker);
 
-    this.setState({ selectedOptionTicker });
-    // this.setState({ test: selectedOptionTicker.target.value });
-    console.log(`Ticker selected:`, selectedOptionTicker);
+  //   this.setState({ selectedOptionTicker });
+  //   // this.setState({ test: selectedOptionTicker.target.value });
+  //   console.log(`Ticker selected:`, selectedOptionTicker);
+  // };
+
+  filterStuff = e => {
+    console.log(this.props);
+    let filteredItems = this.props.tickers.filter(symbol => {
+      if (symbol.name) {
+        return symbol.name.toLowerCase().includes(e.target.value);
+      }
+    });
+    this.setState({
+      filteredItems,
+      search: e.target.value
+    });
+    console.log(filteredItems, filteredItems.length);
   };
 
-  test = e => {
-    console.log(e.target);
-  }
- 
+  showOptions = e => {
+    return this.state.filteredItems.slice(0, 5).map((eachItem, i) => {
+      return (
+        <li
+          key={eachItem.symbol}
+          className="tickerOptions"
+          name={eachItem.name}
+          onClick={() => this.selectItem(eachItem)}
+        >
+          {eachItem.name}
+        </li>
+      );
+    });
+  };
+
+  selectItem = item => {
+    console.log(item);
+    this.setState({
+      searchTicker: item.symbol,
+      searchName: item.name
+    });
+    this.filterStuff({ target: { value: item.name } });
+  };
+
+  showDetails = () => {
+    console.log("oh yeah... we went there....");
+    console.log(this.state.searchTicker);
+     this.setState({ selectedOptionTicker: this.state.search.symbol },
+      ()=>{
+          this.props.history.push(`/Details/${this.state.searchTicker}`);
+      });
+
+  };
+
   render() {
-    //   console.log(this.props)
-    const { selectedOptionTicker } = this.state;
-    
     return (
       <div className="col home ease-in">
         <div className="col-12 home">
@@ -60,19 +104,24 @@ class Home extends Component {
         <br />
         <div className="row">
           <div className="col-6 offset-3">
-            <Form.Group as={Col} controlId="tcformpicker">
-              <Select
-                value={selectedOptionTicker}
-                // onInput={e => this.handleChangeTicker(e)}
-                onChange={e => this.handleChangeTicker(e)}
-                onInput={e => this.test(e)}
-                options={this.showTickerOptions()}
+            <div className="md-form">
+              <input
+                value={this.state.search}
+                type="text"
+                placeholder="Type the Company or ETF name..."
+                className="form-scontrol"
+                onChange={this.filterStuff}
               />
-            </Form.Group>
+              {this.showOptions()}
+            </div>
             <br />
           </div>
         </div>
-        <button type="submit" className="btn btn-success center">
+        <button
+          type="submit"
+          onClick={() => this.showDetails(this.state.search)}
+          className="btn btn-success center"
+        >
           Let's Get Started!
         </button>
       </div>

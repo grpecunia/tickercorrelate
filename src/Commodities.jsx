8b2293b1e1752400ae49f68a71f0e72d9147e323
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-02-25 16:35:25 -0500

    stable changes commit

--- src/Commodities.jsx
+++ src/Commodities.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import { Modal, Button } from "react-bootstrap";
 import axios from "axios";
 import MyChart from "./MyChart";
+import Chart from "react-google-charts";
 
 
 class Commodities extends Component {
-
   componentDidMount() {
     // Axios GET for Ticker Historical Prices @ Market Close Info
     axios
@@ -21,12 +21,6 @@ class Commodities extends Component {
         console.log(res.data.historical);
       });
   }
-
-  // grabCommodity = (commodity) => {
-  //   this.setState({
-  //     dataSet : commodity
-  //   })
-  // }
 
   // Functionality that structures the dataSet needed to populate the ticker/commodity line graph
   structureData = (dataSet, historicalClosePrices) => {
@@ -106,37 +100,110 @@ class Commodities extends Component {
         // return final;
       }
     );
-    this.createCorrDataSet(corrDS)
+    this.createCorrDataSet(corrDS);
+    this.setScatterPlotData(corrDS);
     console.log("Did we made this MFer to a DataSET??? >>>", final);
     this.setState({
-      data: final
+      data: final,
+      corrDS
     });
   };
 
-  createCorrDataSet = (arr) => {
-    console.log('start generating corr DS>>> ', arr)
+  // Functionality to create the Correlation Coefficient dataSet needed to run the Pearson Correlation
+  createCorrDataSet = arr => {
+    console.log("start generating corr DS>>> ", arr);
     let ticketArr = [];
     let commoArr = [];
     // let r, r2;
     arr.forEach(row => {
-      ticketArr.push(row[1])
-      commoArr.push(row[2])
-    })
-    console.log(ticketArr, commoArr)
-    // r = correlation.rank([ticketArr], [commoArr]);
-    // r2 = correlation.determination([ticketArr], [commoArr]);
-    // this.setState({
-    //   r : r,
-    //   r2 : r2
-    // })
-  }
+      ticketArr.push(row[1]);
+      commoArr.push(row[2]);
+    });
+    console.log(ticketArr, commoArr);
+    let r = this.getPearsonCorrelation(ticketArr, commoArr);
+    let rSquared = r * r;
+    console.log(r, rSquared);
+    this.setState({
+      r,
+      rSquared
+    });
+  };
+
+  setScatterPlotData = arr => {
+    let scatterArr = [
+      [`${this.props.match.params.ticker}`, `${this.props.match.params.ticker}`]
+    ];
+    arr.forEach(row => {
+      scatterArr.push([row[1], row[2]]);
+    });
+    console.log(scatterArr);
+    this.setState({
+      dataSP: scatterArr
+    });
+  };
 
   // This is STATE of Commodities
   state = {
     show: false,
     data: [],
     r: 0,
-    r2 : 0,
+    rSquared: 0,
+    dataSP: [],
+    corrDS : []
+  };
+
+  getPearsonCorrelation = (x, y) => {
+    let shortestArrayLength = 0;
+
+    if (x.length === y.length) {
+      shortestArrayLength = x.length;
+    } else if (x.length > y.length) {
+      shortestArrayLength = y.length;
+      console.error(
+        "x has more items in it, the last " +
+          (x.length - shortestArrayLength) +
+          " item(s) will be ignored"
+      );
+    } else {
+      shortestArrayLength = x.length;
+      console.error(
+        "y has more items in it, the last " +
+          (y.length - shortestArrayLength) +
+          " item(s) will be ignored"
+      );
+    }
+
+    let xy = [];
+    let x2 = [];
+    let y2 = [];
+
+    for (let i = 0; i < shortestArrayLength; i++) {
+      xy.push(x[i] * y[i]);
+      x2.push(x[i] * x[i]);
+      y2.push(y[i] * y[i]);
+    }
+
+    let sum_x = 0;
+    let sum_y = 0;
+    let sum_xy = 0;
+    let sum_x2 = 0;
+    let sum_y2 = 0;
+
+    for (let i = 0; i < shortestArrayLength; i++) {
+      sum_x += x[i];
+      sum_y += y[i];
+      sum_xy += xy[i];
+      sum_x2 += x2[i];
+      sum_y2 += y2[i];
+    }
+
+    var step1 = shortestArrayLength * sum_xy - sum_x * sum_y;
+    var step2 = shortestArrayLength * sum_x2 - sum_x * sum_x;
+    var step3 = shortestArrayLength * sum_y2 - sum_y * sum_y;
+    var step4 = Math.sqrt(step2 * step3);
+    var answer = step1 / step4;
+
+    return answer;
   };
 
   // Functionality to Show the Correlation Results Modal
@@ -147,6 +214,18 @@ class Commodities extends Component {
   hideModal = () => {
     this.setState({ show: false });
   };
+
+  handleStartDateChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  handleEndDateChange = (e) => {
+    console.log(e.target.value)
+  };
+
+  sliceCorrDS = () => {
+
+  }
 
   render() {
     console.log(this.state.data);
@@ -169,17 +248,26 @@ class Commodities extends Component {
           >
             <div className="row-3 home">
               <label>Correlation Start Date</label>
-              <input type="date" required></input>
+              <input
+                type="date"
+                required
+                onChange={e => this.handleStartDateChange(e)}
+              ></input>
             </div>
             <div className="row-3 home">
               <label>Correlation End Date</label>
-              <input type="date" required></input>
+              <input
+                type="date"
+                required
+                onChange={e => this.handleEndDateChange(e)}
+              ></input>
             </div>
             <br />
             <Modal
-              size="lg"
+              size="xl"
               aria-labelledby="contained-modal-title-vcenter"
               centered
+              height="90%"
               show={this.state.show}
             >
               <Modal.Header>
@@ -188,25 +276,57 @@ class Commodities extends Component {
                   style={{ textAlign: "center" }}
                 >
                   <b>
-                    {" "}
-                    Correlation Results [ {this.props.match.params.ticker}
-                    {" & "}
-                    {this.props.match.params.com} ]
+                    <span role="img" aria-label="Graph">
+                      📊
+                    </span>{" "}
+                    Pearson Correlation Results for{" "}
+                    {this.props.match.params.ticker}
+                    {" + "}
+                    {this.props.match.params.com}
                   </b>
                 </Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                <h4>Centered Modal</h4>
                 <div className="row">
                   <div className="col-6">
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo
-                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                    risus, porta ac consectetur ac, vestibulum at eros.
+                    <h4 className="home">
+                      r = {this.state.r.toFixed(3)} | r² ={" "}
+                      {this.state.rSquared.toFixed(3)}
+                    </h4>
+                    <p>
+                      Pearson correlation coefficient, also referred to as
+                      Pearson's r, the Pearson product-moment correlation
+                      coefficient or the bivariate correlation, is a measure of
+                      the linear correlation between two variables X and Y.
+                    </p>
                   </div>
                   <div className="col-6">
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo
-                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                    risus, porta ac consectetur ac, vestibulum at eros.
+                    <Chart
+                      width={"100%"}
+                      height={"100%"}
+                      chartType="ScatterChart"
+                      loader={
+                        <div className="loading">
+                          Loading ScaterPlot Chart...
+                        </div>
+                      }
+                      data={this.state.dataSP}
+                      options={{
+                        title: `${this.props.match.params.com} vs. ${this.props.match.params.ticker} comparison`,
+                        hAxis: {
+                          title: `${this.props.match.params.com}`,
+                          minValue: 0,
+                          maxValue: 100
+                        },
+                        vAxis: {
+                          title: `${this.props.match.params.ticker}`,
+                          minValue: 0,
+                          maxValue: 100
+                        },
+                        legend: "none"
+                      }}
+                      rootProps={{ "data-testid": "1" }}
+                    />
                   </div>
                 </div>
               </Modal.Body>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import logo from "./logo.svg";
 
 class Footer extends Component {
     render() {
         return (
           <React.Fragment>
             <div className="footer">
-              Developed by: GRP | All rights reserved ©{" "}
-              {new Date().getFullYear()}
+              Developed by: <a style={{ color:'white'}}href='https://riverapecunia.com' target='_blank' rel='noopener noreferrer'>GRP</a> |{" "}
+              <img alt="TickerCorrelate" src={logo} width="20" height="20" />{" "}
+              TickerCorrelate | All rights reserved © {new Date().getFullYear()}
             </div>
           </React.Fragment>
         );

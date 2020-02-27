e4b412e3b9dd49071120e9e2e68823aa8900f3fb
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-02-26 22:14:29 -0500

    night commit - stable

--- src/APIs.jsx
+++ src/APIs.jsx
@@ -3,23 +3,53 @@ import React, { Component } from 'react';
 class APIs extends Component {
     render() {
         return (
-          <div className="container">
-            <h2 className="home" style={{ paddingBottom: "25px" }}>
+          <div className="container home">
+            <h1 style={{ paddingBottom: "25px" }}>
               Information about TickerCorrelate's API
-            </h2>
+            </h1>
             <div className="row contatiner">
               <div className="col-lg-5 offset-1">
                 <h3>Financial Data API's</h3>
                 <p>
                   The financial data provided by this application is in part
-                  thanks to Fulano deTal.
+                  thanks to Financial Modeling Prep API. The information for the
+                  market data is pulled on a weekly frequency, this means that
+                  every week's latest data point will be that of last market
+                  close price on the previous end of week market close date
+                  price.
+                </p>
+                <br />
+                <p>
+                  For more information about Financial Modeling Prep visit their
+                  website at:{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://financialmodelingprep.com/"
+                  >
+                    https://financialmodelingprep.com
+                  </a>
                 </p>
               </div>
               <div className="col-lg-5 offset-1">
                 <h3>Commodity API's</h3>
                 <p>
                   The Commodity data provided by this application is in part
-                  thanks to Sutano diTel.
+                  thanks Quandl API. You'll find comprehensive guides and
+                  documentation to help you start working with their open API's,
+                  as well as support if you get stuck.
+                </p>
+                <br />
+                <p>
+                  For more information about Quandl and their APIs visit their
+                  website at:{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://quandl.com/"
+                  >
+                    https://quandl.com
+                  </a>
                 </p>
               </div>
             </div>

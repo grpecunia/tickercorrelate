93f68c092cc9b335c9bf73dc31f062cba391f961
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-02-28 07:09:34 -0500

    changes to the correlations.jsx

--- src/CorrelationsJS.jsx
+++ src/CorrelationsJS.jsx
@@ -11,32 +11,35 @@ class CorrelationsJS extends Component {
                 </div>
               </div>
               <div className="row container" style={{ paddingTop: "20px" }}>
-                <div className="col-lg-5 offset-lg-1 home">
-                  <p>
-                    While developing this application we looked into various
-                    ways of determining the Pearson's coefficient with relation
-                    to our analysis. Although we came close to use the BigML
-                    API, we decided to look into the math and try to write a
-                    JavaScript function that would calculate the Pearson's Coefficient of
-                    Correlation (r) and Coefficient of determination (r²).{" "}
-                  </p>
-                  <img
-                    src="https://media.giphy.com/media/2bYewTk7K2No1NvcuK/giphy.gif"
-                    height="100px"
-                    alt="BrainPower"
-                  />
+                <div className="col-lg-10 offset-lg-1 home">
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <p>
+                        While developing this application there was an intense search into various
+                        ways of determining the Pearson's coefficient with
+                        relation to the correlation analysis. Although many API tools are available to the public at large such as, BigML API, the desicion was made to look into the math and try
+                        to write a JavaScript function that would calculate the
+                        Pearson's Coefficient of Correlation (r) and Coefficient
+                        of determination (r²). In the spirit of openess and data
+                        sharing TickerCorreleate decided to publish the function to share and
+                        promote the use and improvement of it.
+                      </p>
+                    </div>
+                    <div className="col-lg-6">
+                      <img
+                        src="https://media.giphy.com/media/2bYewTk7K2No1NvcuK/giphy.gif"
+                        height="225px"
+                        alt="BrainPower"
+                      />
+                    </div>
+                  </div>
 
-                  <p style={{ paddingTop: "15px" }}>
-                    In the spirit of openess and data sharing we decided to
-                    publish the function to share and promote the use and
-                    improvement of it.
-                  </p>
                   <br />
                 </div>
-                <div className="col-lg-5 offset-lg-1">
+                <div className="col-lg-10 offset-lg-1">
                   <iframe
                     title="PearsonsJSFunction"
-                    height="400px"
+                    height="600px"
                     width="100%"
                     src="https://repl.it/@grpecunia/correlationsJS?lite=true"
                     scrolling="no"
